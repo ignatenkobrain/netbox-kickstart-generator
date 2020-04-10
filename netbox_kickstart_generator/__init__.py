ignatenkobrain/netbox-kickstart-generator

commit 4cf8172127ff5c8ff9e31573d5fa71c04765504f

Allow choosing kickstart file
Signed-off-by: Igor Raits <igor@gooddata.com>

netbox_kickstart_generator/__init__.py
@@ -10,7 +10,8 @@ def create_app():
     app.config.from_pyfile("config.py", silent=True)
 
     @app.route("/kickstart")
-    def kickstart():
+    @app.route("/kickstart/<kickstart>")
+    def kickstart(kickstart="default"):
         if "X-System-Serial-Number" not in request.headers:
             abort(400, "No Serial Number")
         nb = pynetbox.api(app.config["NETBOX_URL"], token=app.config["NETBOX_TOKEN"])
@@ -81,6 +82,9 @@ def create_app():
         for i, data in ifcfg.items():
             ifcfg[i].update({"device": i, "name": i, "onboot": "yes"})
 
-        return render_template("test.ks", device=device, ifcfg=ifcfg)
+        try:
+            return render_template(f"{kickstart}.ks", device=device, ifcfg=ifcfg)
+        except jinja2.exceptions.TemplateNotFound:
+            abort(404, "Template not found")
 
     return app

commit a42985ba7c7c08705693247407447675635ed89e

Properly handle missing templates
Fixes: 4cf8172127ff5c8ff9e31573d5fa71c04765504f
Signed-off-by: Igor Raits <igor@gooddata.com>

netbox_kickstart_generator/__init__.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import netaddr
 from flask import Flask, abort, request, render_template
+from flask.templating import TemplateNotFound
 import pynetbox
 
 
@@ -85,7 +86,7 @@ def create_app():
 
         try:
             return render_template(f"{kickstart}.ks", device=device, ifcfg=ifcfg)
-        except jinja2.exceptions.TemplateNotFound:
+        except TemplateNotFound:
             abort(404, "Template not found")
 
     return app